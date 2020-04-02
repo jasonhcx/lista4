9a2ce92272a509cdc4f33c3b4678bce872fa9081
jasonhcx <45397197+jasonhcx@users.noreply.github.com>
2020-04-01 21:31:33 -0300

    Add files via upload

---
+++ emissor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+
+	int client_fd, porta;	  //file descriptor do socket, porta recebida como parametro
+	char buffer;			  //buffer que armazena o caracter a ser enviado
+	char *host = argv[2]; //endereco para quem vai ser enviado o arquivo recebido como parametro
+	struct sockaddr_in server_addr;
+
+	porta = atoi(argv[3]); //porta a ser enviada recebido como parametro
+
+	client_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(porta);
+	inet_aton(host, &server_addr.sin_addr);
+
+	FILE *pf = fopen(argv[1], "r"); //abre o arquivo a ser enviado recebido como parametro
+    if (pf == NULL)
+    {
+    printf("Falha ao abrir o arquivo!\n");
+    }
+
+	while (fread(&buffer, sizeof(char), 1, pf) != 0)
+	{
+		sendto(client_fd, &buffer, 1, 0, (struct sockaddr *)&server_addr,
+			   (unsigned int)sizeof(server_addr));										  
+	}
+
+	sendto(client_fd, NULL, 0, 0, (struct sockaddr *)&server_addr,
+		   (unsigned int)sizeof(server_addr));
+
+	fclose(pf);
+
+	return 0;
+}
---
+++ receptor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+
+	int server_fd, port, recv_bytes;   //file descriptor do socket, porta e bytes recebidos
+	char buffer;     // buffer que armazenara caracter recebido
+	struct sockaddr_in server_addr;
+
+	porta = atoi(argv[1]); //porta a ser enviada recebido como parametro
+
+	server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(porta);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+	FILE *pf = fopen("arquivo_recebido.txt", "wb"); //abre o arquivo a ser escrito
+    if (pf == NULL)
+    {
+    printf("Falha ao abrir o arquivo!\n");
+    }
+
+	do{
+		recv_bytes = recvfrom(server_fd, &buffer, 1, MSG_WAITALL,
+							  NULL, NULL); /
+
+		fwrite(&buffer, sizeof(char), 1, pf);
+	}while(recv_bytes != 0);
+
+	fclose(pf);
+
+	return 0;
+}
